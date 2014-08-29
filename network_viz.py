11272c06584fc2915a60f7431adc995b16ddccc6
wronk <wronk.mark@gmail.com>
2014-08-28 23:12:10 -0700

    Documentation and bug fixes

--- network_viz.py
+++ network_viz.py
@@ -10,71 +10,102 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 
-def plot_3D_network(ont_names, ont_positions, label_set, edges, edge_labels,
-                    node_sizes=1, node_colors='g', edge_sizes=1,
-                    edge_colors='b'):
+def plot_3D_network(node_names, node_positions, node_label_set, edges,
+                    edge_label_set, node_sizes=None, node_colors=None,
+                    edge_sizes=None, edge_colors=None):
     '''
     Plot clustering coefficient probability density function
 
     Parameters
     ----------
-    ont_names : list
+    node_names : list
         matrix of clustering coefficients
-    ont_positions : N x 3 array
+    node_positions : N x 3 array
         array of positions
     edges : list
         connections between nodes
+    node_label_set : list of bool
+        whether or not to text label the nodes
+    edges : list of tuples
+        edges between nodes (use graph.edges())
+    edge_label_set : list of bool
+        whether or not to label the edges
+    node_sizes : list | array of ints
+        size of each node's marker in the graph
+    node_colors : list
+        color of each node's marker
+    edge_sizes : list
+        size of each edge's line
+    edge_colors : list
+        color of each edge's line
 
     Returns
     --------
     fig : fig
         figure object of distribution histogram for plotting
+    ax : ax
+        figure axis
     '''
 
     # Initialize figure
     fig = plt.figure()
-    ax = fig.gca(projection='3d')
-    #ax = fig.add_subplot(111, projection='3d')
-    node_label_offset = 0.1
+    ax = fig.add_subplot(111, projection='3d')
+    node_label_offset = 0.01
     edge_label_offset = 0.05
 
+    ###################################
+    if node_sizes is None:
+        node_sizes = [50] * len(node_names)
+    if node_colors is None:
+        node_colors = ['g'] * len(node_names)
+    if edge_sizes is None:
+        edge_sizes = [1] * len(edges)
+    if edge_colors is None:
+        edge_colors = ['b'] * len(edges)
+
+    ###################################
     # Add nodes to graph
-    ax.scatter(ont_positions[:, 0], ont_positions[:, 1], ont_positions[:, 2],
-               s=node_sizes, c=node_colors, depthshade=False)
-    for ni, node_pt in enumerate(ont_positions):
-        ax.text(ont_positions[ni, 0], ont_positions[ni, 1],
-                ont_positions[ni, 2] + node_label_offset, ont_names[ni],
-                color=node_colors[ni], ha='center')
+    ax.scatter(node_positions[:, 0], node_positions[:, 1],
+               node_positions[:, 2], s=node_sizes, c=node_colors,
+               depthshade=False)
+    for ni, node_pt in enumerate(node_positions):
+        if node_label_set[ni]:
+            ax.text(node_positions[ni, 0], node_positions[ni, 1],
+                    node_positions[ni, 2] + node_label_offset, node_names[ni],
+                    color=node_colors[ni], ha='center')
 
     ###################################
     # Convert edges to list
-    edge_inds = [[ont_names.index(n1), ont_names.index(n2)]
+    edge_inds = [[node_names.index(n1), node_names.index(n2)]
                  for n1, n2 in edges]
-    print edge_inds
 
     # Generate list of 2 x 3 array corresponding to list of end edge points
-    edge_positions = [np.vstack((ont_positions[e[0]], ont_positions[e[1]]))
+    edge_positions = [np.vstack((node_positions[e[0]], node_positions[e[1]]))
                       for e in edge_inds]
-    print edge_positions[0]
 
+    ###################################
     # Add edges to graph
     for ei, edge_pt in enumerate(edge_positions):
         ax.plot(edge_pt[:, 0], edge_pt[:, 1], edge_pt[:, 2],
                 lw=edge_sizes[ei], c=edge_colors[ei],
                 dash_capstyle='round', zorder=-1)
 
-        mean_pt = np.mean(edge_pt, axis=0)
-        vec_dir = edge_pt[1, :] - edge_pt[0, :]
-        ax.text(mean_pt[0], mean_pt[1], mean_pt[2] + edge_label_offset,
-                ont_names[edge_inds[ei][0]] + '<->' + ont_names[edge_inds[ei][1]],
-                vec_dir, color=edge_colors[ei])
+        # Add text edge label to graph
+        if edge_label_set[ei]:
+            mean_pt = np.mean(edge_pt, axis=0)
+            vec_dir = edge_pt[1, :] - edge_pt[0, :]
+
+            ax.text(mean_pt[0], mean_pt[1], mean_pt[2] + edge_label_offset,
+                    node_names[edge_inds[ei][0]] + '<->' +
+                    node_names[edge_inds[ei][1]], vec_dir,
+                    color=edge_colors[ei], ha='center')
 
     # Cleanup
     ax.set_xlabel('X pos')
     ax.set_ylabel('Y pos')
     ax.set_zlabel('Z pos')
+    ax.set_title('Brain Graph')
 
-    plt.show()
     return fig, ax
 
 if __name__ == '__main__':
@@ -105,21 +136,24 @@ if __name__ == '__main__':
                   'data': W_net}
     G = network_gen.import_weights_to_graph(W_net_dict)
 
-    ont_names = row_labels[0:5]
-    ont_positions = np.random.rand(5, 3)
-    ont_labels = [True] * len(ont_names)
+    node_names = row_labels[0:5]
+    node_positions = np.random.rand(5, 3)
+    node_labels = [True] * len(node_names)
     edges = [('AAA_L', 'ACAv_L'), ('AD_L', 'ACAd_L')]
 
-    node_sizes = np.random.randint(20, high=300, size=len(ont_names))
-    node_colors = ['g'] * len(ont_names)
+    node_sizes = np.random.randint(20, high=300, size=len(node_names))
+    node_colors = ['g'] * len(node_names)
 
     edge_sizes = np.random.randint(1, high=4, size=len(edges))
     edge_colors = ['b'] * len(edges)
     edge_labels = [True] * len(edges)
 
-    fig, ax = plot_3D_network(ont_names, ont_positions, ont_labels, edges,
+    fig, ax = plot_3D_network(node_names, node_positions, node_labels, edges,
                               edge_labels, node_sizes, node_colors,
                               edge_sizes, edge_colors)
+    fig, ax = plot_3D_network(node_names, node_positions, node_labels, edges,
+                              edge_labels)
+
 
     plt.draw()
     plt.show()
