22a9dd3db145b551c4a04f64643fd565f35f39bd
wronk <wronk.mark@gmail.com>
2014-08-28 22:42:52 -0700

    added network_viz

--- network_viz.py
+++ network_viz.py
@@ -7,9 +7,12 @@ network_viz.py
 '''
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
-def plot_3D_network(ont_names, ont_positions, edges):
+def plot_3D_network(ont_names, ont_positions, label_set, edges, edge_labels,
+                    node_sizes=1, node_colors='g', edge_sizes=1,
+                    edge_colors='b'):
     '''
     Plot clustering coefficient probability density function
 
@@ -29,18 +32,57 @@ def plot_3D_network(ont_names, ont_positions, edges):
     '''
 
     # Initialize figure
-
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    #ax = fig.add_subplot(111, projection='3d')
+    node_label_offset = 0.1
+    edge_label_offset = 0.05
 
     # Add nodes to graph
+    ax.scatter(ont_positions[:, 0], ont_positions[:, 1], ont_positions[:, 2],
+               s=node_sizes, c=node_colors, depthshade=False)
+    for ni, node_pt in enumerate(ont_positions):
+        ax.text(ont_positions[ni, 0], ont_positions[ni, 1],
+                ont_positions[ni, 2] + node_label_offset, ont_names[ni],
+                color=node_colors[ni], ha='center')
+
+    ###################################
+    # Convert edges to list
+    edge_inds = [[ont_names.index(n1), ont_names.index(n2)]
+                 for n1, n2 in edges]
+    print edge_inds
+
+    # Generate list of 2 x 3 array corresponding to list of end edge points
+    edge_positions = [np.vstack((ont_positions[e[0]], ont_positions[e[1]]))
+                      for e in edge_inds]
+    print edge_positions[0]
 
     # Add edges to graph
+    for ei, edge_pt in enumerate(edge_positions):
+        ax.plot(edge_pt[:, 0], edge_pt[:, 1], edge_pt[:, 2],
+                lw=edge_sizes[ei], c=edge_colors[ei],
+                dash_capstyle='round', zorder=-1)
+
+        mean_pt = np.mean(edge_pt, axis=0)
+        vec_dir = edge_pt[1, :] - edge_pt[0, :]
+        ax.text(mean_pt[0], mean_pt[1], mean_pt[2] + edge_label_offset,
+                ont_names[edge_inds[ei][0]] + '<->' + ont_names[edge_inds[ei][1]],
+                vec_dir, color=edge_colors[ei])
+
+    # Cleanup
+    ax.set_xlabel('X pos')
+    ax.set_ylabel('Y pos')
+    ax.set_zlabel('Z pos')
+
+    plt.show()
+    return fig, ax
 
 if __name__ == '__main__':
 
-    import networkx as nx
-
     import network_gen
+
     plt.close('all')
+    plt.ion()
 
     # Set parameters
     p_th = .01  # P-value threshold
@@ -65,6 +107,19 @@ if __name__ == '__main__':
 
     ont_names = row_labels[0:5]
     ont_positions = np.random.rand(5, 3)
-    edges = []
+    ont_labels = [True] * len(ont_names)
+    edges = [('AAA_L', 'ACAv_L'), ('AD_L', 'ACAd_L')]
 
-    fig = plot_3D_network(ont_names, ont_positions, edges)
+    node_sizes = np.random.randint(20, high=300, size=len(ont_names))
+    node_colors = ['g'] * len(ont_names)
+
+    edge_sizes = np.random.randint(1, high=4, size=len(edges))
+    edge_colors = ['b'] * len(edges)
+    edge_labels = [True] * len(edges)
+
+    fig, ax = plot_3D_network(ont_names, ont_positions, ont_labels, edges,
+                              edge_labels, node_sizes, node_colors,
+                              edge_sizes, edge_colors)
+
+    plt.draw()
+    plt.show()
