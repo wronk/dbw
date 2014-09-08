7c9f20acd353caec8b0c66172d7530276be3b0dd
wronk <wronk.mark@gmail.com>
2014-09-07 17:10:27 -0700

    add gif to repo

--- movie/create_movie.sh
+++ movie/create_movie.sh
@@ -1,3 +1,4 @@
 cd images/
-# mogrify -resize 640x480 *.png
-convert -delay 5 -loop 0 *.png ../movie5.gif
+# image resize is permanant
+#mogrify -resize 875x396 *.png
+convert -delay 8 -loop 0 *.png ../movie6_8ms.gif
