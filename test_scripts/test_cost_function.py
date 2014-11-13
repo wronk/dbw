b44bf7e83f152445c633b3b348489f4fe7b10fe9
Richard Pang <rkp@d-128-95-10-223.dhcp4.washington.edu>
2014-11-12 17:00:29 -0800

    Add script to calculate cost changes from node-swapping with random weighted graph.

--- test_scripts/test_cost_function.py
+++ test_scripts/test_cost_function.py
@@ -9,10 +9,24 @@ Test creation of random weighted undirected graphs.
 import numpy as np
 import extract.brain_graph
 import metrics.weighted_undirected
+import random_graph.weighted_undirected as rg
 
 # Get mouse connectivity graph & distance
-G, W, _, _ = extract.brain_graph.weighted_undirected()
-D, _ = extract.brain_graph.distance_matrix()
+G_brain, W_brain, _, _ = extract.brain_graph.weighted_undirected()
+D_brain, _ = extract.brain_graph.distance_matrix()
+N_brain = W_brain.shape[0]
+N_edges_brain = (W_brain > 0).sum()/2
+L = 1.
+gamma = 1.5
 
 # Calculate swapped-cost distribution for graph
-cost_change_dist = metrics.weighted_undirected.swapped_cost_distr(W,D)
+cost_changes = metrics.weighted_undirected.swapped_cost_distr(W_brain, D_brain)
+
+# Create random biophysical graph with properly sampled weights
+brain_size = np.array([10.,10.,10.])
+G,W,D = rg.biophysical_sample_weights(N=N_brain, N_edges=N_edges_brain, L=L, 
+                                      gamma=gamma, brain_size=brain_size,
+                                      use_brain_weights=True)
+                                      
+# Calculate swapped-cost distribution for graph
+cost_changes_random = metrics.weighted_undirected.swapped_cost_distr(W, D)
