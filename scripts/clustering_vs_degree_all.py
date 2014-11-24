c2626ceaf83df5991acc6043b84b74c4ed695807
Richard Pang <rkp@d-128-95-10-223.dhcp4.washington.edu>
2014-11-24 09:21:46 -0800

    Move some scripts into test_scripts folder. Scripts folder will only include scripts for making figures.

---
+++ scripts/clustering_vs_degree_all.py
@@ -0,0 +1,9 @@
+"""
+Created on Mon Nov 24 09:17:11 2014
+
+@author: rkp
+
+Plot the clustering vs. degree for mouse connectome, standard random graphs,
+and binary undirected biophysical model at multiple gammas.
+"""
+
