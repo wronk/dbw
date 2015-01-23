6a933ab606f591c976546c561427797f95c3e7c7
Richard Pang <rkp@d-128-95-10-223.dhcp4.washington.edu>
2015-01-23 14:32:20 -0800

    Add importable file containing all constants.

---
+++ constants.py
@@ -0,0 +1,14 @@
+"""
+Created on Fri Jan 23 14:26:54 2015
+
+@author: rkp
+
+Constants from data/papers.
+"""
+
+num_brain_nodes = 426
+num_brain_edges_undirected = 7804
+num_brain_edges_directed = 8820
+
+p_brain_edge_undirected = 0.08620822977078155
+p_brain_edge_directed = 0.04860146796270581
