3bae0bc5c36d9b41c9994e98a33337bec5bef146
Richard Pang <rkp@d-128-95-10-223.dhcp4.washington.edu>
2015-02-05 10:16:57 -0800

    Add random graph with no spatial dependence.

--- random_graph/binary_directed.py
+++ random_graph/binary_directed.py
@@ -174,7 +174,7 @@ def biophysical_reverse(N=426, N_edges=8820, L=np.inf, gamma=1.7, brain_size=[7.
     
 def biophysical_reverse_outdegree(N=426, N_edges=8820, L=np.inf, gamma=1.7, brain_size=[7., 7, 7]):
     """Create a biophysically inspired graph. Connection probabilities depend
-    on distance & degree.
+    on outdegree and distance.
     
     Args:
         N: how many nodes
@@ -243,3 +243,131 @@ def biophysical_reverse_outdegree(N=426, N_edges=8820, L=np.inf, gamma=1.7, brai
     np.fill_diagonal(A,0)
         
     return G, A, D
+    
+def biophysical_reverse_outdegree_nonspatial(N=426, N_edges=8820, gamma=1.):
+    """Create a biophysically inspired graph. Connection probabilities depend
+    on outdegree.
+    
+    Args:
+        N: how many nodes
+        N_edges: how many edges
+        gamma: power to raise outdegree to
+    Returns:
+        Networkx graph object"""
+    
+    # Initialize diagonal adjacency matrix
+    A = np.eye(N, dtype=float)
+    
+    # Make graph object
+    G = nx.DiGraph()
+    G.add_nodes_from(np.arange(N))
+    
+    # Randomly add edges
+    edge_ctr = 0
+    while edge_ctr < N_edges:
+        # Update degree list & degree-related probability vector
+        outdegs = A.sum(1).astype(float)
+        outdegs_prob = outdegs.copy()
+        
+        # Pick random node to draw edge to
+        to_idx = np.random.randint(low=0, high=N)
+        
+        # Skip this node if already fully connected
+        if outdegs[to_idx] == N:
+            continue
+        
+        # Find unavailable cxns and set their probability to zero
+        unavail_mask = A[:,to_idx] > 0
+        outdegs_prob[unavail_mask] = 0
+        # Set self cxn probability to zero
+        outdegs_prob[to_idx] = 0
+        
+        # Calculate cxn probabilities from degree & distance
+        P = outdegs_prob**gamma
+        # On the off changes that P == 0, skip
+        if P.sum() == 0:
+            continue
+        # Otherwise keep going on
+        P /= float(P.sum()) # Normalize probabilities to sum to 1
+            
+        # Sample node from distribution
+        from_idx = np.random.choice(np.arange(N),p=P)
+        
+        # Add edge to graph
+        if A[from_idx,to_idx] == 0:
+            G.add_edge(from_idx,to_idx)
+            
+        # Add edge to adjacency matrix
+        A[from_idx,to_idx] += 1
+        
+        # Increment edge counter
+        edge_ctr += 1
+    
+    # Set diagonals to zero
+    np.fill_diagonal(A,0)
+        
+    return G
+    
+def biophysical_reverse_outdegree_reciprocal(N=426, N_edges=8820, gamma=1.):
+    """Create a biophysically inspired graph. Connection probabilities depend
+    on outdegree.
+    
+    Args:
+        N: how many nodes
+        N_edges: how many edges
+        gamma: power to raise outdegree to
+    Returns:
+        Networkx graph object"""
+    
+    # Initialize diagonal adjacency matrix
+    A = np.eye(N, dtype=float)
+    
+    # Make graph object
+    G = nx.DiGraph()
+    G.add_nodes_from(np.arange(N))
+    
+    # Randomly add edges
+    edge_ctr = 0
+    while edge_ctr < N_edges:
+        # Update degree list & degree-related probability vector
+        outdegs = A.sum(1).astype(float)
+        outdegs_prob = outdegs.copy()
+        
+        # Pick random node to draw edge to
+        to_idx = np.random.randint(low=0, high=N)
+        
+        # Skip this node if already fully connected
+        if outdegs[to_idx] == N:
+            continue
+        
+        # Find unavailable cxns and set their probability to zero
+        unavail_mask = A[:,to_idx] > 0
+        outdegs_prob[unavail_mask] = 0
+        # Set self cxn probability to zero
+        outdegs_prob[to_idx] = 0
+        
+        # Calculate cxn probabilities from degree & distance
+        P = outdegs_prob**gamma
+        # On the off changes that P == 0, skip
+        if P.sum() == 0:
+            continue
+        # Otherwise keep going on
+        P /= float(P.sum()) # Normalize probabilities to sum to 1
+            
+        # Sample node from distribution
+        from_idx = np.random.choice(np.arange(N),p=P)
+        
+        # Add edge to graph
+        if A[from_idx,to_idx] == 0:
+            G.add_edge(from_idx,to_idx)
+            
+        # Add edge to adjacency matrix
+        A[from_idx,to_idx] += 1
+        
+        # Increment edge counter
+        edge_ctr += 1
+    
+    # Set diagonals to zero
+    np.fill_diagonal(A,0)
+        
+    return G
