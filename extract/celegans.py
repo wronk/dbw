63bf8278be742aa3591264efea32e3bc959d7100
Rich Pang <rpang.contact@gmail.com>
2015-02-07 16:39:05 -0800

    Add code to extract celegans DiGraph.

--- extract/celegans.py
+++ extract/celegans.py
@@ -5,3 +5,22 @@ Created on Sat Feb 7 16:23:43 2014
 
 Functions to extract celegans connectome from mat file.
 """
+
+import networkx as nx
+import scipy.io as sio
+
+CELEGANS_FILE = '../data/celegans277.mat'
+
+def binary_directed():
+    """Make directed graph from C. elegans adjacency matrix."""
+    
+    # load all data
+    D = sio.loadmat(CELEGANS_FILE)
+    
+    # get adjacency matrix
+    A = D['celegans277matrix']
+    
+    # create networkx graph
+    G = nx.from_numpy_matrix(A, create_using=nx.DiGraph())
+    
+    return G
