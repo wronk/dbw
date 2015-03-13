fa8e75e38a19c2beea13c12cab1d718e0dacaa2a
Rich Pang <rpang.contact@gmail.com>
2015-03-12 19:02:22 -0700

    Add function to swap positions of two nodes in graph with distances as edge attributes.

--- graph_tools/auxiliary.py
+++ graph_tools/auxiliary.py
@@ -7,6 +7,7 @@ Auxiliary tools for handling graphs.
 """
 
 import numpy as np
+import networkx as nx
 
 def dist_mat(centroids):
     """Compute a distance matrix (returned in mm) from 3D centroids.
@@ -23,3 +24,28 @@ def dist_mat(centroids):
             D[r_idx,c_idx] = d
 
     return D
+    
+def swap_node_positions(G, D, idx0=None, idx1=None):
+    """Swap two nodes' positions and return the resulting graph and distance
+    matrix."""
+    
+    Gswapped = G.copy()
+    
+    # randomly pick nodes to swap if not provided
+    if idx0 is None and idx1 is None:
+        idx0, idx1 = np.random.permutation(len(G.nodes()))[:2]
+        
+    # create new distance matrix
+    Dswapped = D.copy()
+    # swap rows
+    Dswapped[[idx0,idx1],:] = Dswapped[[idx1,idx0],:]
+    # swap columns
+    Dswapped[:,[idx0,idx1]] = Dswapped[:,[idx1,idx0]]
+    
+    # make dictionary of edge distances
+    dd = {edge:Dswapped[edge] for edge in G.edges()}
+    
+    # set edge distances in Gswapped
+    nx.set_edge_attributes(Gswapped, 'distance', dd)
+    
+    return Gswapped, Dswapped
