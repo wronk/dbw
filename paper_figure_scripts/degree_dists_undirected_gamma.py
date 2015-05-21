722ec33f542673aa0bd6d1303a32b69bfa51fc16
wronk <wronk.mark@gmail.com>
2015-05-21 00:15:47 -0700

    Plotting!!!

--- paper_figure_scripts/degree_dists_undirected_gamma.py
+++ paper_figure_scripts/degree_dists_undirected_gamma.py
@@ -24,6 +24,8 @@ GAMMAS = [1., 1.33, 1.67, 2.0]  # Preferential attachment parameters
 #GAMMAS = [1.33, 1.67]  # Preferential attachment parameters
 BRAIN_SIZE = [7., 7., 7.]  # Size brain region volume to distribute nodes
 colors = ['0.1', '0.25', '0.5', '0.75']
+alphas = [0.55, 0.7, 0.85, 1.]
+BRAIN_COLOR = 'm'
 
 plt.ion()
 plt.close('all')
@@ -31,7 +33,7 @@ plt.rcParams['ps.fonttype'] = 42
 plt.rcParams['pdf.fonttype'] = 42
 
 n_bins = 50
-repeats = 100
+repeats = 1
 
 ###############################################
 # Histogram plot function
@@ -40,11 +42,11 @@ repeats = 100
 # reused for similar plots that will have just a change in x or y scale (log)
 
 
-def hist_plot(ax, deg_dists, colors, gammas):
-    for deg, col, g in zip(deg_dists, colors, gammas):
+def hist_plot(ax, deg_dists, colors, gammas, alphas):
+    for deg, col, g, alpha in zip(deg_dists, colors, gammas, alphas):
         hist, plt_bins = np.histogram(deg, lin_bins, normed=True)
-        ax.plot(plt_bins[:-1], hist, lw=2, color=col,
-                label='Gamma = ' + str(g))
+        ax.plot(plt_bins[:-1], hist, lw=2, color=col, label=str(g),
+                alpha=alpha)
 
     # Set axis limits and ticks, and label subplots
     ax.set_xlim([0, 150])
@@ -71,6 +73,7 @@ def hist_plot(ax, deg_dists, colors, gammas):
 G_brain, _, _ = extract.brain_graph.binary_undirected()
 n_nodes = len(G_brain.nodes())
 n_edges = len(G_brain.edges())
+brain_degree = G_brain.degree().values()
 
 # Initialize repetition matrices for standard graphs
 gamma_mat = -1 * np.ones((len(GAMMAS), repeats, n_nodes))
@@ -85,39 +88,39 @@ for gamma_idx, gamma in enumerate(GAMMAS):
 gamma_dists = gamma_mat.reshape((len(GAMMAS), -1))
 
 ###################################################
-# Plot semilogy (looking for exponential solutions)
+# Plot all panels that will have axis scales changes
 ###################################################
-lin_bins = np.linspace(0, 150, n_bins)
-
 figsize = (16, 5)
 fig, axs = plt.subplots(1, 3, figsize=figsize)
+lin_bins = np.linspace(0, 150, n_bins)
 
-hist_plot(axs[0], gamma_dists, colors, GAMMAS)
-axs[0].locator_params(axis='y', nbins=5)
+for ax in axs:
+    ax.hist(brain_degree, lin_bins, color=BRAIN_COLOR, normed=True,
+            label='Mouse\nConnectome', lw=0.5, alpha=.8)
+    hist_plot(ax, gamma_dists, ['c'] * 4, GAMMAS, alphas)
 
 ###################################################
+# Plot specific scales
+###################################################
+
 # Plot semilogy (looking for exponential solutions)
-###################################################
-hist_plot(axs[1], gamma_dists, colors, GAMMAS)
+axs[0].locator_params(axis='y', nbins=6)
 
+# Plot semilogy (looking for exponential solutions)
 axs[1].set_ylim([10E-5, 1])
 axs[1].set_yscale('log')
-axs[1].set_ylabel('Log[P(k)]')
-#axs[1].legend_.remove()
+axs[1].set_ylabel('P(k)')
+axs[1].legend_.remove()
 
-###################################################
 # Plot on log scale (looking for power-law
-###################################################
-hist_plot(axs[2], gamma_dists, colors, GAMMAS)
-
 axs[2].set_xlim([1, 150])
 axs[2].set_ylim([10E-5, 1])
 axs[2].set_xscale('log')
 axs[2].set_yscale('log')
-axs[2].set_xlabel('Log[Degree]')
-axs[2].set_ylabel('Log[P(k)]')
-#axs[2].legend_.remove()
+axs[2].set_xlabel('Degree')
+axs[2].set_ylabel('P(k)')
+axs[2].legend_.remove()
 
 plt.tight_layout()
-fig.subplots_adjust(wspace=0.325, top=.925, bottom=.17)
+fig.subplots_adjust(wspace=0.325, top=.85, bottom=.17)
 plt.draw()
