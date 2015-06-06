1fda1b041cc1818171175936e8781b807ce4f6e1
geebs <sindre.henriksen.09@aberdeen.ac.uk>
2015-06-05 22:55:48 -0400

    My massive contribution; enjoy...

---
+++ color_scheme.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+SMALLWORLD = np.array([17,119,51])/255.
+RANDOM = np.array([204,102,119])/255.
+SCALEFREE = np.array([51,34,136])/255.
+PREFGROWTH = np.array([68,170,53])/255.
+PREFATTACHMENT = np.array([221,204,119])/255.
+CONFIG = np.array([153,153,51])/255.
+ATLAS = np.array([170,68,153])/255.
+PGPA = np.array([136,204,238])/255.
