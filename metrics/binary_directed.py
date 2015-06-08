7410b43950b0760daca14858ed3c2d2f01ac888e
wronk <wronk.mark@gmail.com>
2015-06-07 22:49:04 -0700

    ENH: added direct global and local efficiency calcs

--- metrics/binary_directed.py
+++ metrics/binary_directed.py
@@ -13,29 +13,86 @@ import networkx as nx
 
 def reciprocity(G):
     """Calculate the reciprocity coefficient of a directed graph.
-    
+
     Reciprocity is defined as (Ndirected - Nundirected)/Nundirected"""
-    
+
     # get number of directed edges
     Ndirected = len(G.edges())
-    
+
     # get number of undirected edges
     Nundirected = len(G.to_undirected().edges())
-    
+
     # return reciprocity
     return (Ndirected - Nundirected) / float(Nundirected)
 
+
 def efficiency_matrix(G):
-    """Calculate the efficiency (the inverse of the length of the shortest directed path) for all pairs of nodes in a graph.
-        Rows correspond to source nodes, columns to target nodes."""
+    """Calculate the efficiency (the inverse of the length of the shortest
+    directed path) for all pairs of nodes in a graph.  Rows correspond to
+    source nodes, columns to target nodes.
+
+    Parameters
+    ----------
+    G : networkX graph
+
+    Returns
+    -------
+    efficiencty : ndarray, shape(G.number_of_nodes(), G.number_of_nodes()
+        Matrix containing pairwise efficiency calculations"""
+
+    assert G.is_directed is False, 'Graph can\'t be directed'
 
     shortest_path_lengths = nx.shortest_path_length(G)
 
-    efficiency = np.zeros((len(G.nodes()), len(G.nodes())), dtype=float)
+    # Initialize to nan so that errors are easier to spot
+    efficiency = np.empty((len(G.number_of_nodes()), len(G.number_of_nodes())))
+    efficiency[:] = np.nan
 
     for src in shortest_path_lengths.keys():
         for targ in shortest_path_lengths[src].keys():
-            if src is not targ:
-                efficiency[src, targ] = 1 / shortest_path_lengths[src][targ]
+            if src != targ:
+                efficiency[src, targ] = 1. / shortest_path_lengths[src][targ]
+
+    # Error check for problems in calculation
+    assert np.is_nan(efficiency).any() is False, 'NaN in efficiency matrix'
 
     return efficiency
+
+
+def local_efficiency(G):
+    """Calculate local efficiency for a graph. Local efficiency is the
+    average efficiency of all neighbors of the given node.
+
+    Parameters
+    ----------
+    G : networkX graph
+
+    Returns
+    -------
+    float : avg local efficiency of the graph
+    """
+
+    assert G.is_directed is False, 'Graph can\'t be directed'
+
+    return sum(global_efficiency(G.subgraph[v]) for v in G) / len(G)
+
+
+def global_efficiency(G):
+    """Calculate global efficiency for a graph. Global efficiency is the
+    average efficiency of all neighbors of the given node.
+
+    Parameters
+    ----------
+    G : networkX graph
+
+    Returns
+    -------
+    float : avg global efficiency of the graph
+    """
+
+    assert G.is_directed is False, 'Graph can\'t be directed'
+
+    n_nodes = G.number_of_nodes()
+    den = n_nodes * (n_nodes - 1)
+
+    return np.sum(efficiency_matrix(G)) / den
