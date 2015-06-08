ae9df34d3c6ba62c7d833fb6f80597466cff9811
wronk <wronk.mark@gmail.com>
2015-06-08 00:55:15 -0700

    Fixed labels

--- paper_figure_scripts/clustering_vs_degree_brain_standard_graphs.py
+++ paper_figure_scripts/clustering_vs_degree_brain_standard_graphs.py
@@ -10,23 +10,27 @@ import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 import extract.brain_graph
+import config
 
 from network_plot.change_settings import set_all_text_fontsizes, set_all_colors
 
 # PLOTTING PARAMETERS
-FACECOLOR = 'w'
+FACECOLOR = config.FACE_COLOR
 FIGSIZE = (12, 3.5)
-FONTSIZE = 18
-BRAIN_COLOR = 'm'
-CM_COLOR = 'r'
-WS_COLOR = 'g'
-BA_COLOR = 'b'
-MODEL_COLOR = 'c'
+FONT_SIZE = config.FONT_SIZE
+BRAIN_COLOR = config.COLORS['brain']
+RAND_COLOR = config.COLORS['configuration']
+WS_COLOR = config.COLORS['small-world']
+BA_COLOR = config.COLORS['scale-free']
 DEG_MAX = 150
 DEG_TICKS = [0, 50, 100, 150]
 CC_TICKS = [0, .2, .4, .6, .8, 1.0]
+graph_names = ['Mouse', 'Random', 'Small-world', 'Scale-free']
+labels = ['c', 'd', 'e', 'f']  # Upper corner labels for each plot
 plt.ion()
 plt.close('all')
+
+########################################################
 
 # Load mouse connectivity graph
 G_brain, W_brain, _ = extract.brain_graph.binary_undirected()
@@ -55,7 +59,10 @@ G_BA = nx.barabasi_albert_graph(n_nodes, int(round(brain_degree_mean / 2.)))
 BA_degree = nx.degree(G_BA).values()
 BA_clustering = nx.clustering(G_BA).values()
 
-# Make 8 clustering vs. degree plots
+############
+# Plot
+############
+# Make clustering vs. degree plots
 fig, axs = plt.subplots(1, 4, facecolor=FACECOLOR, figsize=FIGSIZE,
                         tight_layout=True)
 
@@ -63,37 +70,31 @@ fig, axs = plt.subplots(1, 4, facecolor=FACECOLOR, figsize=FIGSIZE,
 axs[0].scatter(brain_degree, brain_clustering, color=BRAIN_COLOR)
 
 # Standard random graphs
-axs[1].scatter(CM_degree, CM_clustering, color=CM_COLOR)
+axs[1].scatter(CM_degree, CM_clustering, color=RAND_COLOR)
 axs[2].scatter(WS_degree, WS_clustering, color=WS_COLOR)
 axs[3].scatter(BA_degree, BA_clustering, color=BA_COLOR)
 
 # Set axis limits and ticks, and label subplots
-labels = ('a', 'b', 'c', 'd')
 for ax_idx, ax in enumerate(axs.flatten()):
     ax.set_xlim(0, DEG_MAX)
     ax.set_ylim(0, 1)
     ax.set_xticks(DEG_TICKS)
     ax.set_yticks(CC_TICKS)
-    ax.text(10, .88, labels[ax_idx], color='k', fontsize=FONTSIZE,
-            fontweight='bold')
+    ax.text(.08, .87, labels[ax_idx], color='k', fontsize=FONT_SIZE,
+            fontweight='bold', transform=ax.transAxes)
+
+    ax.set_xlabel('Degree')
+    set_all_text_fontsizes(ax, FONT_SIZE)
+    set_all_colors(ax, 'k')
+
+    # Set titles
+    ax.set_title(graph_names[ax_idx], fontsize=FONT_SIZE)
 
 # Hide x ticklabels in top row & y ticklabels in right columns
 for ax in axs[1:]:
     ax.set_yticklabels('')
 
 axs[0].set_ylabel('Clustering\ncoefficient')
-
-# Set xlabels
-for ax in axs:
-    ax.set_xlabel('Degree')
-    set_all_text_fontsizes(ax, FONTSIZE)
-    set_all_colors(ax, 'k')
-
-# Set titles
-axs[0].set_title('Mouse')
-axs[1].set_title('Random')
-axs[2].set_title('Small-World')
-axs[3].set_title('Scale-Free')
 
 fig.subplots_adjust(wspace=0.18)
 
