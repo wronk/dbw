af708dfd11b51d4dc668edcfb5d66839f253808d
wronk <wronk.mark@gmail.com>
2015-06-30 22:01:13 -0700

    ENH: Random deg controlled added

--- test_scripts/test_random_directed_degree_sequence.py
+++ test_scripts/test_random_directed_degree_sequence.py
@@ -6,23 +6,31 @@ import matplotlib.pyplot as plt
 
 from random_graph import binary_directed
 from extract import brain_graph
+import networkx as nx
 
-
-G_brain, A_brain, _ = brain_graph.binary_directed()
+G_brain, _, _ = brain_graph.binary_directed()
 
 in_sequence = G_brain.in_degree().values()
 out_sequence = G_brain.out_degree().values()
 
-n_edges_random = []
-for _ in range(N_TRIALS):
-    G, A, _ = binary_directed.random_directed_deg_seq(in_sequence,
-                                                      out_sequence,
-                                                      simplify=True)
-    n_edges_random += [len(G.edges())]
+n_edges_deg_controlled_random = []
+n_edges_ER_random = []
+
+for ri in range(N_TRIALS):
+    G_RAND, _, _ = binary_directed.random_directed_deg_seq(in_sequence,
+                                                           out_sequence,
+                                                           simplify=True)
+    G_ER = nx.random_graphs.erdos_renyi_graph(G_brain.number_of_nodes(),
+                                              p=0.15, directed=True)
+
+    n_edges_deg_controlled_random.append(G_RAND.number_of_edges())
+    n_edges_ER_random.append(G_ER.number_of_edges())
+    print('Finished repeat: ' + str(ri + 1) + ' of ' + str(N_TRIALS))
 
 _, ax = plt.subplots(1, 1)
-ax.hist(n_edges_random, color='b')
+ax.hist(n_edges_deg_controlled_random, color='b', alpha=0.5)
+ax.hist(n_edges_ER_random, color='r', alpha=0.5)
 ax.axvline(len(G_brain.edges()), color='k')
-ax.legend(['random', 'brain'])
+ax.legend(['Deg. Cont. Rand', 'ER Rand.', 'Brain'])
 ax.set_xlabel('number of edges in graph')
 plt.show(block=True)
