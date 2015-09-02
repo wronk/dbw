5bf3f3889bf5cc3762ea06460f12f63ffc770284
geebs <sindre.henriksen.09@aberdeen.ac.uk>
2015-09-02 19:09:10 -0400

    made it prettier

--- scripts/get_reciprocity_values.py
+++ scripts/get_reciprocity_values.py
@@ -6,7 +6,6 @@ import os
 
 from extract.brain_graph import binary_directed as brain_graph
 from random_graph.binary_directed import biophysical_reverse_outdegree as biophysical
-from random_graph.binary_directed import biophysical_reverse_outdegree2 as biophysical2
 
 from network_plot.change_settings import set_all_text_fontsizes, set_all_colors
 
@@ -32,8 +31,7 @@ NBINS = 15
 Ls = np.linspace(0,2,21)
 N = 10
 
-reciprocity_matrix1 = np.zeros([len(Ls),N])
-reciprocity_matrix2 = np.zeros([len(Ls),N])
+reciprocity_matrix = np.zeros([len(Ls),N])
 
 for i,L in enumerate(Ls):
     print "Running L " + str(i) + " of " + str(len(Ls))
@@ -43,20 +41,13 @@ for i,L in enumerate(Ls):
                                N_edges=bc.num_brain_edges_directed,
                                L=L,\
                                gamma=1.)
-
-        G2_PA,A2,_ = biophysical2(N=bc.num_brain_nodes,\
-                                  N_edges=bc.num_brain_edges_directed,
-                                  L=L,\
-                                  gamma=1.)
         
-        reciprocity_matrix1[i,j] = reciprocity(A)
-        reciprocity_matrix2[i,j] = reciprocity(A2)
+        reciprocity_matrix[i,j] = reciprocity(A)
 
-current_reciprocity1_DF = pd.DataFrame(reciprocity_matrix1)
-current_reciprocity2_DF = pd.DataFrame(reciprocity_matrix2)
+current_reciprocity\_DF = pd.DataFrame(reciprocity_matrix)
+
 
 
 data_dir = os.getenv('DBW_DATA_DIRECTORY')
-current_reciprocity1_DF.to_csv(data_dir+'/reciprocity1.csv')
-current_reciprocity2_DF.to_csv(data_dir+'/reciprocity2.csv')
+current_reciprocity_DF.to_csv(data_dir+'/reciprocity.csv')
 
