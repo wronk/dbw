d4154a42e67e7b28435f334ddcfda5b02719402d
wronk <wronk.mark@gmail.com>
2015-09-02 09:48:51 -0700

    Cleaning up labels, corrected `L`

--- paper_figure_scripts/reciprocity_distance_model_vs_atlas.py
+++ paper_figure_scripts/reciprocity_distance_model_vs_atlas.py
@@ -40,71 +40,77 @@ G_shuffled = nx.configuration_model(G.degree().values())
 
 Ls = np.linspace(0,2,21)
 
-# Load the saved reciprocity values (this is from like 200 runs of the model, which
-# takes forever to run..
+# Load the saved reciprocity values (this is from like 200 runs of the model,
+# which takes forever to run..
 data_dir=os.getenv('DBW_DATA_DIRECTORY')
-modelReciprocity = pd.read_csv(data_dir+'/reciprocity2.csv',index_col=0).as_matrix()
+modelReciprocity = pd.read_csv(data_dir+'/reciprocity2.csv',
+                               index_col=0).as_matrix()
 meanReciprocity = np.mean(modelReciprocity,axis=1)
 stdReciprocity = np.std(modelReciprocity,axis=1)
 
 brainReciprocity = reciprocity(A)
+n_repeats = 20
 
 # Get a mean value for the shuffled connectome
-configReciprocity = np.zeros([20,1])
+configReciprocity = np.zeros([n_repeats,1])
 # First get lists of in and out deg to feed to config function
 nodes = G.nodes()
 inDeg = G.in_degree(); inDeg = [inDeg[node] for node in nodes]
 outDeg = G.out_degree(); outDeg = [outDeg[node] for node in nodes]
 # Loop over 20 (to get a good mean value and SD estimate)
-for j in range(20):
+for j in range(n_repeats):
     G_config = nx.directed_configuration_model(inDeg,outDeg)
     A_config = nx.adjacency_matrix(G_config)
     A_configInt = np.zeros(A_config.shape,dtype=int)
 
     # Was having some weird difficulties with this
     A_configInt[A_config < 0.5] = 0
-    A_configInt[A_config > 0.5] = 1
-    
+    A_configInt[A_config >= 0.5] = 1
+
     configReciprocity[j] = reciprocity(A_configInt)
 
 
 # Now plot everything
 # First plot mean reciprocity and fill in +/- 1 SD
 axs[0].plot(Ls[1:],meanReciprocity[1:],linewidth=2,color=color_scheme.PGPA)
-axs[0].fill_between(Ls[1:],meanReciprocity[1:]-stdReciprocity[1:],meanReciprocity[1:]+stdReciprocity[1:],\
-                 facecolor=color_scheme.PGPA,alpha=0.2,antialiased=True,linewidth=3,linestyle='-',
-                 edgecolor=color_scheme.PGPA)
+axs[0].fill_between(Ls[1:],meanReciprocity[1:]-stdReciprocity[1:],
+                    meanReciprocity[1:]+stdReciprocity[1:],
+                    facecolor=color_scheme.PGPA,
+                    alpha=0.2,antialiased=True,linewidth=3,linestyle='-',
+                    edgecolor=color_scheme.PGPA)
 
 # Plot brain reciprocity
-axs[0].plot([0,2],[brainReciprocity,brainReciprocity],linewidth=4,color=color_scheme.ATLAS,linestyle='--')
+axs[0].plot([0,2],[brainReciprocity,brainReciprocity],linewidth=4,
+            color=color_scheme.ATLAS,linestyle='--')
 
 # Plot configuration reciprocity
-configX = [0,2]; configY = [np.mean(configReciprocity),np.mean(configReciprocity)]
+configX = [0,2]; configY = [np.mean(configReciprocity),
+                            np.mean(configReciprocity)]
 axs[0].plot(configX,configY,linewidth=2,color=color_scheme.CONFIG)
-axs[0].fill_between(configX,configY-np.std(configReciprocity),configY+np.std(configReciprocity),\
-                 facecolor=color_scheme.CONFIG,alpha=0.2,antialiased=True,linewidth=3,linestyle=':',\
-                 edgecolor=color_scheme.CONFIG)
+axs[0].fill_between(configX,configY-np.std(configReciprocity),
+                    configY+np.std(configReciprocity),
+                    facecolor=color_scheme.CONFIG,alpha=0.2,antialiased=True,
+                    linewidth=3,linestyle=':', edgecolor=color_scheme.CONFIG)
 
 
 # Set ticks, labels, and font sizes
 xticks = np.arange(0,2.5,0.5)
 yticks = [0,0.1,0.2,0.3]
 axs[0].set_xlabel('L (mm)',fontsize=labelsize,color='k')
-axs[0].set_ylabel('Reciprocity',fontsize=labelsize,color='k')
+axs[0].set_ylabel('Reciprocity coefficient',fontsize=labelsize,color='k')
 axs[0].set_xticks(xticks)
 axs[0].set_yticks(yticks)
 axs[0].set_xticklabels(xticks,fontsize=labelsize,color='k')
 axs[0].set_yticklabels(yticks,fontsize=labelsize,color='k')
 axs[0].set_xlim([Ls[1],2])
 axs[0].set_ylim([0,0.3])
-leg = axs[0].legend(['PGPA model', 'Allen Atlas', 'Shuffled Atlas'],prop={'size':22})
+leg = axs[0].legend(['PGPA model', 'Allen Atlas', 'Shuffled Atlas'],
+                    prop={'size':22})
 
 
-
-
-
-
-
+##################################
+# Get reciprocity
+##################################
 
 centroidsUncorrected = aux_random_graphs.get_coords()
 # This is because the coordinates are off by a factor of 10
@@ -155,15 +161,17 @@ axs[1].set_xticklabels(xticks,fontsize=labelsize)
 axs[1].set_yticks(yticks)
 axs[1].set_yticklabels(yticks,fontsize=labelsize)
 axs[1].set_xlabel('Distance (mm)', fontsize=labelsize)
-axs[1].set_ylabel('Density', fontsize=labelsize)
+axs[1].set_ylabel('Probability density', fontsize=labelsize)
 axs[1].set_xlim([0,max(bins)])
+axs[1].set_title('Connectome Reciprocity')
 axs[1].legend(['Reciprocal edges', 'All edges'],prop={'size':legendsize})
 
-### Now for the model...
+##################################
+# Now for the biophysical model...
+##################################
 G, A, labels = biophysical_model(N=num_brain_nodes,
                                  N_edges=num_brain_edges_directed,
-                                 L=.7,
-                                 gamma=1., brain_size='brain')
+                                 L=.75, gamma=1., brain_size='brain')
 
 centroids = G.centroids
 
@@ -201,14 +209,15 @@ proportionRecip = binnedRecipDistances[0]
 
 
 axs[2].hist(edges.values(),bins,normed=True,facecolor=color_scheme.PGPA)
-axs[2].plot(bins[0:len(bins)-1],proportionRecip,linewidth=3,color='k',linestyle='-')            
+axs[2].plot(bins[0:len(bins)-1],proportionRecip,linewidth=3,color='k',linestyle='-')
 axs[2].set_xticks(xticks)
 axs[2].set_xticklabels(xticks,fontsize=labelsize)
 axs[2].set_yticks(yticks)
 axs[2].set_yticklabels([])
 axs[2].set_xlabel('Distance (mm)', fontsize=labelsize)
-#axs[2].set_ylabel('Density', fontsize=28)
+axs[2].set_ylabel('Probability density', fontsize=28)
 axs[2].set_xlim([0,10.0])
+axs[2].set_title('PGPA Reciprocity')
 axs[2].legend(['Reciprocal edges', 'All edges'],prop={'size':legendsize})
 
 
